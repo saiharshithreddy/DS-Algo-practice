2d47635900a5c698ae6763f182c9b92434d1b2a3
saiharshithreddy <shrshthreddy@gmail.com>
2020-02-21 11:39:14 -0800

    ebay interview ques

--- Python/DynamicProgramming/equal-partition-sum.py
+++ Python/DynamicProgramming/equal-partition-sum.py
@@ -1,3 +1,12 @@
+'''
+Approach:
+Difficulties faced:
+How did I resolve them:
+Time complexity: O()
+Space complexity: O()
+
+Algorithm:
+'''
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         sum = sum(nums)
@@ -6,3 +15,4 @@ class Solution:
             return False
 
         dp = [-1]
+        ssds
